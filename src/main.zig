b72ae2d9c21ab10fe7e55846dd395304d0d43aa8
Paul Kukowski <paul.kukowski@webid-solutions.de>
2025-02-27 13:38:30 +0100

    Milestone: rotating cube

--- src/main.zig
+++ src/main.zig
@@ -51,6 +51,138 @@ pub fn makeProgram() !gl.GLuint {
     return program;
 }
 
+pub fn makeVao() !gl.GLuint {
+    const vertices: []const f32 = &.{
+        0.5, 0.5, -0.5,
+        0.5, -0.5, -0.5,
+        0.5, 0.5, 0.5,
+        0.5, -0.5, 0.5,
+        -0.5, 0.5, -0.5,
+        -0.5, -0.5, -0.5,
+        -0.5, 0.5, 0.5,
+        -0.5, -0.5, 0.5,
+    };
+
+    const position_indices: []const u32 = &.{
+        4, 2, 0,
+        2, 7, 3,
+        6, 5, 7,
+        1, 7, 5,
+        0, 4, 1,
+        4, 1, 5,
+        4, 6, 2,
+        2, 6, 7,
+        6, 4, 5,
+        1, 3, 7,
+        0, 2, 3,
+        4, 0, 1,
+    };
+
+    const normal_indices: []const u32 = &.{
+        0, 0, 0,
+        1, 1, 1,
+        2, 2, 2,
+        3, 3, 3,
+        4, 4, 4,
+        5, 5, 5,
+        0, 0, 0,
+        1, 1, 1,
+        2, 2, 2,
+        3, 3, 3,
+        4, 4, 4,
+        5, 5, 5,
+    };
+
+    const normals: []const f32 = &.{
+        0.0, 1.0, 0.0,
+        0.0, 0.0, 1.0,
+        -1.0, 0.0, 0.0,
+        0.0, -1.0, 0.0,
+        1.0, 0.0, 0.0,
+        0.0, 0.0, -1.0,
+    };
+
+    var combined_buffer: [36 * 3 * 2]f32 = undefined;
+    var combined_buffer_idx: usize = 0;
+
+    for (position_indices, normal_indices) |vidx, nidx| {
+        defer combined_buffer_idx += 6;
+        const vert = vertices[vidx * 3..(vidx+1)*3];
+        const norm = normals[nidx * 3..(nidx+1)*3];
+
+        combined_buffer[combined_buffer_idx + 0] = vert[0];
+        combined_buffer[combined_buffer_idx + 1] = vert[1];
+        combined_buffer[combined_buffer_idx + 2] = vert[2];
+        combined_buffer[combined_buffer_idx + 3] = norm[0];
+        combined_buffer[combined_buffer_idx + 4] = norm[1];
+        combined_buffer[combined_buffer_idx + 5] = norm[2];
+    }
+
+    var vao: gl.GLuint = 0;
+    var vbo: gl.GLuint = 0;
+
+    gl.glGenVertexArrays(1, &vao);
+    gl.glGenBuffers(1, &vbo);
+
+    gl.glBindVertexArray(vao);
+
+    gl.glBindBuffer(gl.GL_ARRAY_BUFFER, vbo);
+    gl.glBufferData(gl.GL_ARRAY_BUFFER, combined_buffer.len * @sizeOf(f32), &combined_buffer, gl.GL_STATIC_DRAW);
+
+    gl.glVertexAttribPointer(0, 3, gl.GL_FLOAT, gl.GL_FALSE, 6 * @sizeOf(f32), null);
+    gl.glEnableVertexAttribArray(0);
+
+    gl.glBindBuffer(gl.GL_ARRAY_BUFFER, 0);
+
+    return vao;
+}
+
+fn matMul(a: [16]f32, b: [16]f32) [16]f32 {
+    var ret: [16]f32 = .{0} ** 16;
+    for (0..16) |idx| {
+        const row = idx / 4;
+        const col = idx % 4;
+
+        for (0..4) |mat_idx| {
+            ret[idx] += a[row * 4 + mat_idx] * b[mat_idx * 4 + col];
+        }
+    }
+    return ret;
+}
+
+const identity: [16]f32 = .{
+    1, 0, 0, 0,
+    0, 1, 0, 0,
+    0, 0, 1, 0,
+    0, 0, 0, 1,
+};
+
+fn makeZRotation(amount: f32) [16]f32 {
+    const sx = @sin(amount);
+    const cx = @cos(amount);
+
+    var matrix = identity;
+    matrix[0] = cx;
+    matrix[1] = -sx;
+    matrix[4] = sx;
+    matrix[5] = cx;
+
+    return matrix;
+}
+
+fn makeYRotation(amount: f32) [16]f32 {
+    const sx = @sin(amount);
+    const cx = @cos(amount);
+
+    var matrix = identity;
+    matrix[0] = cx;
+    matrix[2] = -sx;
+    matrix[8] = sx;
+    matrix[10] = cx;
+
+    return matrix;
+}
+
 pub fn main() !void {
     _ = glfw.glfwSetErrorCallback(errorCallback);
 
@@ -62,7 +194,7 @@ pub fn main() !void {
     glfw.glfwWindowHint(glfw.GLFW_CONTEXT_VERSION_MINOR, 5);
     glfw.glfwWindowHint(glfw.GLFW_OPENGL_PROFILE, glfw.GLFW_OPENGL_CORE_PROFILE);
 
-    const window = glfw.glfwCreateWindow(800, 600, "ziggorama", null, null);
+    const window = glfw.glfwCreateWindow(800, 800, "ziggorama", null, null);
     if (window == null) {
         return error.InitWindow;
     }
@@ -70,11 +202,47 @@ pub fn main() !void {
     glfw.glfwMakeContextCurrent(window);
 
     const program = try makeProgram();
-    _ = program;
+    const vao = try makeVao();
+
+    gl.glEnable(gl.GL_DEPTH_TEST);
+    gl.glDepthFunc(gl.GL_LESS);
+
+    const camera_uniform = gl.glGetUniformLocation(program, "camera");
+    const translation: [16]f32 = .{
+        1.0, 0.0, 0.0, 0.1,
+        0.0, 1.0, 0.0, 0.0,
+        0.0, 0.0, 1.0, 2.0,
+        0.0, 0.0, 0.0, 1.0,
+    };
+
+    var last = try std.time.Instant.now();
+    var y_rot: f32 = 0.5;
 
     while (glfw.glfwWindowShouldClose(window) == 0) {
-        gl.glClearColor(0.2, 0.3, 0.3, 1.0);
-        gl.glClear(gl.GL_COLOR_BUFFER_BIT);
+        const now = try std.time.Instant.now();
+        defer last = now;
+        var elapsed: f32 = @floatFromInt(now.since(last));
+        elapsed /= std.time.ns_per_s;
+
+        gl.glClearColor(0.1, 0.2, 0.3, 1.0);
+        gl.glClearDepth(std.math.inf(f32));
+        gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT);
+
+        y_rot += elapsed * 2 * std.math.pi * 0.25;
+        while (y_rot > 2 * std.math.pi) {
+            y_rot -= 2 * std.math.pi;
+        }
+
+        const x = matMul(
+            makeYRotation(y_rot),
+            makeZRotation(0.5),
+        );
+        const camera_matrix = matMul(translation, x);
+
+        gl.glUseProgram(program);
+        gl.glUniformMatrix4fv(camera_uniform, 1, gl.GL_TRUE, &camera_matrix);
+        gl.glBindVertexArray(vao);
+        gl.glDrawArrays(gl.GL_TRIANGLES, 0, 36);
 
         glfw.glfwSwapBuffers(window);
         glfw.glfwPollEvents();
--- src/vs.glsl
+++ src/vs.glsl
@@ -2,6 +2,15 @@
 
 layout (location = 0) in vec3 aPos;
 
+mat4x4 perspective = mat4x4(
+  vec4(1.5, 0, 0, 0),
+  vec4(0, 1.5, 0, 0),
+  vec4(0, 0, 1, 1),
+  vec4(0, 0, -2 * 0.01, 0)
+);
+
+uniform mat4x4 camera;
+
 void main() {
-  gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+  gl_Position = perspective * camera * vec4(aPos.x, aPos.y, aPos.z, 1.0);
 }
